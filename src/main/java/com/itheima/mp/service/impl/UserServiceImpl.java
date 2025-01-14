acc423e571127fadb6a17a116095cfe6076eda15
liangxinyu <a1242753955@163.com>
2025-01-14 17:16:50 +0800

    MybatisPlus_learning-枚举处理器

--- src/main/java/com/itheima/mp/service/impl/UserServiceImpl.java
+++ src/main/java/com/itheima/mp/service/impl/UserServiceImpl.java
@@ -8,6 +8,7 @@ import com.itheima.mp.domain.po.Address;
 import com.itheima.mp.domain.po.User;
 import com.itheima.mp.domain.vo.AddressVO;
 import com.itheima.mp.domain.vo.UserVO;
+import com.itheima.mp.enums.UserStatus;
 import com.itheima.mp.mapper.UserMapper;
 import com.itheima.mp.service.IUserService;
 import org.springframework.context.annotation.Bean;
@@ -26,7 +27,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         //1.查询用户
         User user = getById(id);
         //2.校验用户状态
-        if(user == null || user.getStatus() == 2){
+        if(user == null || user.getStatus() == UserStatus.FROZEN){
             throw new RuntimeException("用户状态异常！");
         }
         //3.校验余额是否充足
@@ -38,7 +39,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         int remainBalance = user.getBalance() - money;
         lambdaUpdate()
                 .set(User::getBalance,remainBalance)
-                .set(remainBalance==0,User::getStatus,2)
+                .set(remainBalance==0,User::getStatus,UserStatus.FROZEN)
                 .eq(User::getId,id)
                 .eq(User::getBalance,user.getBalance()) //乐观锁
                 .update();
@@ -59,7 +60,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
     public UserVO queryUserAndAddressById(Long id) {
         //1.查询用户
         User user = getById(id);
-        if(user==null || user.getStatus()==2){
+        if(user==null || user.getStatus()==UserStatus.FROZEN){
             throw new RuntimeException("用户状态异常！");
         }
         //2.查询地址
